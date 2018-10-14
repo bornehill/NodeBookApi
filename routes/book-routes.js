3c78624466dc94b3b83a7ff1973a86b3238af94b
bornehill <ali.escamilla.navarro@gmail.com>
2018-10-14 00:05:53 -0500

    add patch, delete & middleware

--- routes/book-routes.js
+++ routes/book-routes.js
@@ -16,23 +16,82 @@ const routes = () => {
             if(err)
             {
                 resp.status(500).send(err);
+                return;
             }
     
             resp.json(books);
         });
     });
     
-    bookRouter.route('/:bookId')
-    .get((req, resp) => {
+    bookRouter.use('/:bookId', (req, resp, next) => {
         Book.findById(req.params.bookId, (err, book) => {
             if(err)
             {
                 resp.status(500).send(err);
+                return;
             }
+
+            if(!book)
+            {
+                resp.status(404).send('Book does not exist');
+                return;
+            }            
     
-            resp.json(book);
-        });
+            req.book = book;
+            next();
+        });        
     });
+
+    bookRouter.route('/:bookId')
+    .get((req, resp) => {
+        resp.json(req.book);
+    })
+    .put((req, resp) => {
+            const book = req.book;            
+            book.title = req.body.title;
+            book.author = req.body.author;
+            book.genre = req.body.genre;
+            book.read = req.body.read;
+            book.save((err) => {
+                if(err)
+                {
+                    resp.status(500).send(err);
+                    return;
+                }
+    
+                resp.json(book);
+            });
+    })
+    .patch((req, resp) => {
+        if(req.body._id)
+            delete req.body._id;
+        
+        for(let p in req.body)
+        {
+            req.book[p] = req.body[p];
+        }
+
+        req.book.save((err) => {
+            if(err)
+            {
+                resp.status(500).send(err);
+                return;
+            }
+
+            resp.json(req.book);
+        });
+    })
+    .delete((req, resp) => {
+        req.book.remove((err) => {
+            if(err)
+            {
+                resp.status(500).send(err);
+                return;
+            }
+
+            resp.status(204).send('Removed');
+        });
+    })    ;
 
     return bookRouter;
 };
